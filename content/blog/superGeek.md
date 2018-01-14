95e30b283942440b5c0cd7befd74108220c85d88
Alexis Janvier <contact@alexisjanvier.net>
2018-01-14 14:55:46 +0100

    List posts

---
+++ content/blog/superGeek.md
@@ -0,0 +1,15 @@
+---
+order: 1
+title: 'Super Geek'
+picture: 'supergeek.png'
+description: 'Un super collaborateur de rubber ducking'
+type: poupée
+tags:
+    - volante
+---
+
+Un an déja
+
+#### La classe
+
+Avec lui aucun bug ne me résiste.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <div>
     <h1>Hello</h1>
     <p>Bienvenue sur mon site de couture.</p>
+    <h4>{data.allMarkdownRemark.totalCount} travail en stock</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            {node.frontmatter.title}
+          </h3>
+          <p>{node.frontmatter.description}</p>
+        </div>
+      ))}
   </div>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`;
